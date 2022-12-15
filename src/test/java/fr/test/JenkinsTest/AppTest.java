94a3f829521228d8c330804cca176ffa659c8e8a
Morgan MINBIELLE <hornitzaile@hotmail.fr>
2022-12-15 09:58:22 +0100

    Projet Maven type.

---
+++ src/test/java/fr/test/JenkinsTest/AppTest.java
@@ -0,0 +1,20 @@
+package fr.test.JenkinsTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
